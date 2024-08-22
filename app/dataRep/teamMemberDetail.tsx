5f8e52091c5dde22b2a79f5de999ca05df106799
Seán O'Sullivan <seangerardosullivan@gmail.com>
2024-08-22 11:26:20 +0100

    not sure why

--- app/dataRep/teamMemberDetail.tsx
+++ app/dataRep/teamMemberDetail.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, Dimensions, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { LineChart } from 'react-native-chart-kit';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../unused/NavigationTypes';
 import { useSensorData } from '@/context/SensorDataContext';
-import { TeamDataRow } from '@/scripts/fetchTeamDataBeta';
 import TeamMember from '@/components/teamMember';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter, useLocalSearchParams } from 'expo-router';
+import { Skia, Canvas, Path } from '@shopify/react-native-skia';
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -18,11 +17,8 @@ const TeamMemberDetail: React.FC = () => {
   const route = useRoute<TeamMemberDetailRouteProp>();
   const { playerName } = route.params;
   const { teamData, loading } = useSensorData();
-  const [memberData, setMemberData] = useState<TeamDataRow | null>(null);
-  const [accelerationData, setAccelerationData] = useState<number[]>([]);
-  const [historicalData, setHistoricalData] = useState<TeamDataRow | null>(null);
-  const [historicalAccelerationData, setHistoricalAccelerationData] = useState<number[]>([]);
-  const [showGyroscope, setShowGyroscope] = useState(false);
+  const [memberData, setMemberData] = useState<number[]>([]);
+  const [historicalData, setHistoricalData] = useState<number[]>([]);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   const { historical } = useLocalSearchParams();
@@ -34,12 +30,10 @@ const TeamMemberDetail: React.FC = () => {
         const previousSessions = JSON.parse(await AsyncStorage.getItem('previousSessions') || '[]');
         const sessionData = previousSessions.find((session: any) => session.sessionName === currentSession);
         if (sessionData) {
-          const memberData = sessionData.data.find((row: TeamDataRow) => row[0] === playerName);
-          console.log(memberData[6]);
+          const memberData = sessionData.data.find((row: any) => row[0] === playerName);
           if (memberData) {
             const validAccels = memberData[6].filter((value: any) => !isNaN(value) && isFinite(value));
-            setHistoricalData(memberData);
-            setHistoricalAccelerationData(validAccels);
+            setHistoricalData(validAccels);
           } else {
             setError('Historical member data not found.');
           }
@@ -52,43 +46,32 @@ const TeamMemberDetail: React.FC = () => {
     getHistoricalData();
   }, [historical, playerName]);
 
-  if(!historical){
   useEffect(() => {
-    const updateData = () => {
-      const memberData = teamData.find(row => row[0] === playerName);
-      if (memberData) {
-        const validAccels = memberData[6]?.filter(value => !isNaN(value) && isFinite(value));
-        setAccelerationData(validAccels || []);
-        setMemberData(memberData);
-      } else {
-        setError('Current member data not found.');
-        setAccelerationData([]);
-        setMemberData(null);
-      }
-    };
+    if (!historical) {
+      const updateData = () => {
+        const memberData = teamData.find(row => row[0] === playerName);
+        if (memberData) {
+          const validAccels = memberData[6]?.filter(value => !isNaN(value) && isFinite(value));
+          setMemberData(validAccels || []);
+        } else {
+          setError('Current member data not found.');
+          setMemberData([]);
+        }
+      };
 
-    // Initial data load
-    updateData();
+      updateData();
+      const intervalId = setInterval(updateData, 50);
 
-    // Set up interval for updating data
-    const intervalId = setInterval(updateData, 2000); // Update every second
-
-    // Clear interval on component unmount
-    return () => clearInterval(intervalId);
-  }, [teamData, playerName]);}
-
-  const toggleDataSet = () => {
-    setShowGyroscope(prevState => !prevState);
-  };
+      return () => clearInterval(intervalId);
+    }
+  }, [teamData, playerName]);
 
   if (loading) {
     return (
       <View style={styles.waitingContainer}>
         <TouchableOpacity
           style={styles.homeButton}
-          onPress={() => {
-            router.push('/dataRep/teamPage')
-          }}
+          onPress={() => router.push('/dataRep/teamPage')}
         >
           <Ionicons name={historical ? "arrow-back" : "home"} size={24} color="white" />
         </TouchableOpacity>
@@ -102,9 +85,7 @@ const TeamMemberDetail: React.FC = () => {
       <View style={styles.waitingContainer}>
         <TouchableOpacity
           style={styles.homeButton}
-          onPress={() => {
-            router.push('/dataRep/teamPage')
-          }}
+          onPress={() => router.push('/dataRep/teamPage')}
         >
           <Ionicons name={historical ? "arrow-back" : "home"} size={24} color="white" />
         </TouchableOpacity>
@@ -113,196 +94,56 @@ const TeamMemberDetail: React.FC = () => {
     );
   }
 
+  const displayData = historical ? historicalData : memberData;
 
-  const displayData = historical ? historicalAccelerationData : accelerationData;
-  const displayMemberData = historical ? historicalData : memberData;
-  const dataToDisplay = displayMemberData ? (displayMemberData[6]) : [0, 0, 0, 0];
-  const dataToDisplayG = displayMemberData ? (displayMemberData[7]) : [0, 0, 0, 0];
-
-  if (!dataToDisplay || dataToDisplay.length === 0) {
+  if (!displayData || displayData.length === 0) {
     return (
       <View style={styles.waitingContainer}>
         <TouchableOpacity
           style={styles.homeButton}
-          onPress={() => {
-            router.push('/dataRep/teamPage')
-          }}
+          onPress={() => router.push('/dataRep/teamPage')}
         >
           <Ionicons name={historical ? "arrow-back" : "home"} size={24} color="white" />
         </TouchableOpacity>
-        <Text style={styles.waitingText}>No data available for {showGyroscope ? 'gyroscope' : 'acceleration'} values.</Text>
+        <Text style={styles.waitingText}>No data available for acceleration values.</Text>
       </View>
     );
   }
 
+  const path = Skia.Path.Make();
+  path.moveTo(0, displayData[0]);
 
-// Add a null check for dataToDisplayG before slicing and filtering
-  let filteredDataG: number[] = [];
-  let filteredLabelsG: string[] = [];
-
-  if (dataToDisplayG && dataToDisplayG.length > 0) {
-   filteredDataG = dataToDisplayG.slice(-100);
-   filteredLabelsG = dataToDisplayG
-  .slice(-100)
-  .map((value, index) => {
-    if (value !== 0 && index % 10 === 0) {
-      return `${index}`;
-    } else {
-      return ''; // Empty string for labels not to be rendered
-    }
-  })
-  .filter(label => label !== null);
-}
-
-
-  const filteredData = dataToDisplay.slice(-100);
-  const filteredLabels = dataToDisplay
-  .slice(-100)
-  .map((value, index) => {
-    if (value !== 0 && index % 10 === 0) {
-      return `${index}`;
-    } else {
-      return ''; // Empty string for labels not to be rendered
-    }
-  })
-  .filter(label => label !== null);
-
-
-  const chartData = {
-    labels: filteredLabels,
-    datasets: [
-      {
-        data: filteredData,
-        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // White line
-        strokeWidth: 2, // Optional line width
-      },
-    ],
-  };
-
-  const chartDataG = {
-    labels: filteredLabelsG,
-    datasets: [
-      {
-        data: filteredDataG,
-        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // White line
-        strokeWidth: 2, // Optional line width
-      },
-    ],
-  };
-
-  const chartConfig = {
-    backgroundGradientFrom: 'orange',
-    backgroundGradientTo: 'red',
-    backgroundGradientFromOpacity: 1,
-    backgroundGradientToOpacity: 1,
-    useShadowColorFromDataset: false, // Removes the semi-transparent fill under the line
-    decimalPlaces: 2, // Optional, defaults to 2dp
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // White labels
-    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    style: {
-      borderRadius: 16,
-    },
-    propsForBackgroundLines: {
-      stroke: 'transparent', // Makes the background lines invisible
-    },
-    propsForDots: {
-      r: '0.1',
-      strokeWidth: '0.1',
-      stroke: '#ffffff',
-    },
-  };
-
-
-  const chartConfigGyro = {
-    backgroundGradientFrom: 'green',
-    backgroundGradientTo: 'blue',
-    backgroundGradientFromOpacity: 1,
-    backgroundGradientToOpacity: 1,
-    useShadowColorFromDataset: false, // Removes the semi-transparent fill under the line
-    decimalPlaces: 2, // Optional, defaults to 2dp
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // White labels
-    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    style: {
-      borderRadius: 16,
-    },
-    propsForBackgroundLines: {
-      stroke: 'transparent', // Makes the background lines invisible
-    },
-    propsForDots: {
-      r: '0.1',
-      strokeWidth: '0.1',
-      stroke: '#ffffff',
-    },
-  };
+  displayData.forEach((value, index) => {
+    const x = (index / displayData.length) * screenWidth;
+    const y = 50 - value; // Adjust as per your y-axis scaling
+    path.lineTo(x, y);
+  });
 
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
-      <TouchableOpacity
+        <TouchableOpacity
           style={styles.homeButton}
-          onPress={() => {
-            router.push('/dataRep/teamPage')
-          }}
+          onPress={() => router.push('/dataRep/teamPage')}
         >
           <Ionicons name={historical ? "arrow-back" : "home"} size={24} color="white" />
         </TouchableOpacity>
-        {/* <Text style={styles.headerText}>{sessionName || 'Your Team'}</Text> */}
-        <View style={styles.homeButtonPlaceholder} />
         <Text style={styles.headerText}>{playerName}</Text>
       </View>
       <View style={styles.chartContainer}>
-        {displayMemberData && (
-          <TeamMember
-            playerName={displayMemberData[0]}
-            number1={displayMemberData[1]}
-            number2={displayMemberData[2]}
-            number3={displayMemberData[3]}
-            risk={displayMemberData[4]}
-            accels={displayMemberData[6]}
-          />
-        )}
+        <TeamMember
+          playerName={String(playerName)}  // Ensure playerName is a string
+          number1={displayData[1]}
+          number2={displayData[2]}
+          number3={displayData[3]}
+          risk={String(displayData[4])}    // Convert risk to string
+          accels={Array.isArray(displayData[6]) ? displayData[6] : [displayData[6]]}  // Ensure accels is an array
+        />
       </View>
-      {/* <Button title={`Show ${showGyroscope ? 'Acceleration' : 'Gyroscope'} Values`} onPress={toggleDataSet} /> */}
-
-      {/* Replace the ScrollView and table with the LineChart */}
-      <Text style={styles.headerText}>Linear Acceleration Values: </Text>
-      <LineChart
-        data={chartData}
-        yAxisLabel="  "
-       yAxisSuffix=""
-        yAxisInterval={1}
-        width={screenWidth} // Width of the chart
-        height={220} // Height of the chart
-        chartConfig={chartConfig}
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-          paddingLeft: 40, // Add padding to the left to make room for y-axis labels
-          paddingRight: 40,
-        }}
-      />
-      {/* <ScrollView style={styles.scrollView}>
-      {filteredData.map((value, index) => (
-        <Text key={index} style={styles.dataText}>
-          {value}
-        </Text>
-      ))}
-    </ScrollView> */}
-      {/* <Text style={styles.headerText}>Angular Acceleration Values: </Text>
-      <LineChart
-        data={chartDataG}
-        width={screenWidth - 50} // Width of the chart
-        height={220} // Height of the chart
-        chartConfig={chartConfigGyro}
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-          paddingLeft: 20, // Add padding to the left to make room for y-axis labels
-          paddingRight: 20,
-        }}
-      /> */}
-
-      {/* Padding for layout adjustment */}
+      <Text style={styles.headerText}>Linear Acceleration Values:</Text>
+      <Canvas style={{ width: screenWidth, height: 250 }}>
+        <Path path={path} color="#c43a31" style="stroke" strokeWidth={2} />
+      </Canvas>
       <View style={{ paddingBottom: 80 }}></View>
     </View>
   );
@@ -314,15 +155,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#242424',
     padding: 10,
     paddingTop: 50,
-  },
-  scrollView: {
-    marginVertical: 20,
-    maxHeight: 200,  // Adjust this as needed to control the height of the ScrollView
-  },
-  dataText: {
-    fontSize: 16,
-    color: '#E0E0E0',
-    padding: 5,
   },
   headerContainer: {
     margin: 10,
@@ -358,9 +190,6 @@ const styles = StyleSheet.create({
   homeButton: {
     padding: 10,
   },
-  homeButtonPlaceholder: {
-    width: 24, // to align the center text properly
-  }
 });
 
 export default TeamMemberDetail;
--- context/SensorDataContext.tsx
+++ context/SensorDataContext.tsx
@@ -51,13 +51,13 @@ export const SensorDataProvider: React.FC<SensorDataProviderProps> = ({ children
           setLoading(false);
         }
       } catch (error) {
-        // console.error('Error fetching team data:', error);
+        console.error('Error fetching team data:', error);
         setLoading(false);
       }
     };
-
+~
     fetchData();
-    const intervalId = setInterval(fetchData, 3100);
+    const intervalId = setInterval(fetchData, 500);
 
     return () => clearInterval(intervalId);
   }, []);
